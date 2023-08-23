88aa829cf8cfa9d3a01503080cc048aa26462e4f
Grigory <lololohka057@gmail.com>
2023-08-23 17:12:04 +0300

    test_30

--- main/forms.py
+++ main/forms.py
@@ -6,13 +6,8 @@ class StyleFormMixin:
       super().__init__(*args, **kwargs)
       for field_name, field in self.fields.items():
          field.widget.attrs['class'] = 'form-control'
-      
 
 class ProductForm(forms.ModelForm):
-   
-   def __init__(self, *args, **kwargs):
-      super().__init__(*args, **kwargs)
-      self.fields['user'].disabled = True
 
    class Meta:
       model = Product 
